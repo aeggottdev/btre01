aa64da151b63c8e6b14477fece6d1ea314e9622b
Angel Gottfriedt <aeg.dev.gott@gmail.com>
2018-12-28 14:10:58 -0300

    finish styling Listings, Realtors admin area

--- listings/admin.py
+++ listings/admin.py
@@ -2,4 +2,15 @@ from django.contrib import admin
 from .models import Listing
 # Register your models here.
 
-admin.site.register(Listing)
+class ListingAdmin(admin.ModelAdmin):
+    list_display = ('id','title','is_publish','price','list_date','realtor')
+    list_display_links=('id','title')
+    list_filter= ('realtor',)
+    list_editable=('is_publish',)
+    list_per_page=25
+    search_fields=('title','price','list_date','realtor')
+    
+
+
+
+admin.site.register(Listing,ListingAdmin)
--- realtors/admin.py
+++ realtors/admin.py
@@ -1,4 +1,9 @@
 from django.contrib import admin
 from .models import Realtor
 # Register your models here.
-admin.site.register(Realtor)
+class RealtorAdmin(admin.ModelAdmin):
+    list_display=('id','name','email','hire_date')
+    list_display_links=('id','name')
+    list_per_page= 25
+    search_fields=('name','email')
+admin.site.register(Realtor,RealtorAdmin)
