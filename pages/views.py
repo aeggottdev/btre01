fe24815bfc9d0cd478572679af657429aea8d68f
Angel Gottfriedt <aeg.dev.gott@gmail.com>
2018-12-28 21:34:03 -0300

    added dynamic data fetching to about us page -  created the mvp seller logic for the about page

--- pages/views.py
+++ pages/views.py
@@ -1,10 +1,25 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+from listings.models import Listing
+from realtors.models import Realtor
+
 # Create your views here.
 
 def index(request):
-    return render(request, 'pages/index.html')
+    listings = Listing.objects.order_by('-list_date').filter(is_publish=True)[:3]
+    context =  {
+        'listings' : listings
+    }
+    return render(request, 'pages/index.html', context)
 
 def about(request):
-    return render(request, 'pages/about.html')
+    #get al realtor
+    realtors = Realtor.objects.order_by('-hire_date')
+    #get realtor of the onth
+    mvps = Realtor.objects.all().filter(is_mvp=True)
+    context = {
+        'realtors': realtors,
+        'mvps': mvps
+    }
+    return render(request, 'pages/about.html', context)
--- realtors/admin.py
+++ realtors/admin.py
@@ -2,8 +2,9 @@ from django.contrib import admin
 from .models import Realtor
 # Register your models here.
 class RealtorAdmin(admin.ModelAdmin):
-    list_display=('id','name','email','hire_date')
+    list_display=('id','name','is_mvp','email','hire_date')
     list_display_links=('id','name')
+    list_editable=('is_mvp',)
     list_per_page= 25
     search_fields=('name','email')
 admin.site.register(Realtor,RealtorAdmin)
